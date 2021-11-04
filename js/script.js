1cfb33081c774ee7e5bcfb931083a938dc595c34
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-04 15:58:21 +0100

    creation of member arrays

--- js/script.js
+++ js/script.js
@@ -2,5 +2,70 @@
 // - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
 // Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
-// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team
 
+
+// 1. Creare l'array contente i membri del team
+
+// 2. Stampare i membri in HTML
+
+
+
+const members = 
+
+[
+    { 
+        "nome" : 'Weyne Barnett',
+        "ruolo" : 'Founder &amp CEO',
+        "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    { 
+        "nome" : 'Angela Caroll',
+        "ruolo" : 'Chief Editor',
+        "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    { 
+        "nome" : 'Walter Gordon',
+        "ruolo" : 'Office Manager',
+        "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    { 
+        "nome" : 'Angela Lopez',
+        "ruolo" : 'Social Media Manager',
+        "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    { 
+        "nome" : 'Scott Estrada',
+        "ruolo" : 'Developer',
+        "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    { 
+        "nome" : 'Barbara Ramos',
+        "ruolo" : 'Graphic Designer',
+        "foto" : 'img/wayne-barnett-founder-ceo.jpg'
+    },
+]
+
+
+for (let i = 0; i < members.length; i++) {
+
+    let nameMembro = '';
+    let ruoloMembro = '';
+    let fotoMembro = '';
+
+    for (var key in members) {
+
+        nameMembro = members[i].nome;
+        ruoloMembro = members[i].ruolo;
+        fotoMembro = members[i].foto;
+    }
+
+    console.log(nameMembro);
+    console.log(ruoloMembro);
+    console.log(fotoMembro);
+}
